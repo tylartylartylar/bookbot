ccc39c5e6b2a082b4bb8fda03d0278910ceaa441
Tylar <tylar@Tylars-MacBook-Pro.local>
2025-01-27 16:47:15 -0600

    Added word count function and character count function to count words and characters in the document

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ def main():
     with open ("./books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    wordCount(file_contents)
+    characterCount = charCount(file_contents)
+    wordCount = wordCounter(file_contents)
+    print_report(wordCount, characterCount)
+
+def print_report(words, characters):
+    print('----- Beginning report on Frankenstein -----')
+    print(f"there are {words} in the document")
     
-def wordCount(bookText):
+    for char in characters:
+        print(f"{characters[char]} is present {char} times")
+
+def wordCounter(bookText):
     wordCountStr = bookText.split()
     totalWords = len(wordCountStr)
-    print(f"{totalWords} words found in the document.")
+    return f"{totalWords} words found in the document."
+
+def charCount(bookText):
+    character_dic = {}
+    lowercaseContents = bookText.lower()
+
+    for char in lowercaseContents:
+        if char in character_dic:
+            character_dic[char] += 1
+        else: 
+            character_dic[char] = 1
+    return character_dic
+
+
 
 main()
