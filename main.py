7aba7a5edac6de14ae2e3097acc8e942f388306d
Tylar <tylar@Tylars-MacBook-Pro.local>
2025-01-28 14:26:11 -0600

    Completed report building function in main.py

--- main.py
+++ main.py
@@ -4,31 +4,44 @@ def main():
 
     characterCount = charCount(file_contents)
     wordCount = wordCounter(file_contents)
+
     print_report(wordCount, characterCount)
 
+# This doesn't work. This should print a nicely formatted overview of word count, character counts sorted by number of occurances.
 def print_report(words, characters):
-    print('----- Beginning report on Frankenstein -----')
-    print(f"there are {words} in the document")
+    print('--- Begin report of books/frankenstein.txt ---')
+    print(f"{words}\n")
+    for char_tuple in characters:
+        letter = char_tuple[0]
+        count = char_tuple[1]
+        print(f"The '{letter}' character was found {count} times")
     
-    for char in characters:
-        print(f"{characters[char]} is present {char} times")
+    print('--- End report ---')
 
+
+# 
 def wordCounter(bookText):
-    wordCountStr = bookText.split()
-    totalWords = len(wordCountStr)
-    return f"{totalWords} words found in the document."
+    wordCountStr = bookText.split() # Creates a list of strings (words) found in the bookText
+    totalWords = len(wordCountStr)  # The length of the list
+    return f"{totalWords} words found in the document" # Returns the length of the list.
 
+
+# ===== Get Character Count =====
 def charCount(bookText):
-    character_dic = {}
-    lowercaseContents = bookText.lower()
+    character_dic = {} # define the dictionary for character and it's count.
+    lowercaseContents = bookText.lower() # formats all chars into lowercase. 
 
     for char in lowercaseContents:
         if char in character_dic:
             character_dic[char] += 1
         else: 
             character_dic[char] = 1
-    return character_dic
+    
+    filtered_dic = {k: v for k, v in character_dic.items() if k.isalpha()}
+    sorted_dic = sorted(filtered_dic.items(), key=lambda x: x[1], reverse=True)
 
+    return sorted_dic
+# ===== Get Character Count =====
 
 
 main()
